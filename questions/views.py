0ddfb7de1c95d632ec1e41671b784d918538e543
nabeel-trojan <nabeelhashim.trojan@gmail.com>
2025-06-22 17:11:11 +0400

    updating api fixes

--- questions/views.py
+++ questions/views.py
@@ -1,9 +1,18 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters as drf_filters
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as df_filters
 from rest_framework.permissions import AllowAny
 
 from .models import Question
 from .serializers import QuestionSerializer
+
+
+class QuestionFilter(df_filters.FilterSet):
+    class Meta:
+        model = Question
+        fields = ['question_type', 'department']
+        # Exclude JSONFields: 'courses', 'roles', 'options', 'correct_answer'
+
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
@@ -12,8 +21,8 @@ class QuestionViewSet(viewsets.ModelViewSet):
     """
     serializer_class = QuestionSerializer
     permission_classes = [AllowAny]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['question_type', 'department', 'courses', 'roles']
+    filter_backends = [DjangoFilterBackend, drf_filters.SearchFilter, drf_filters.OrderingFilter]
+    filterset_class = QuestionFilter
     search_fields = ['content', 'department']
     ordering_fields = ['created_at', 'updated_at', 'duration']
     ordering = ['-created_at']
